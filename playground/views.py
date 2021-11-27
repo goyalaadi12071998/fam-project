7f254ce4a193a6a917f224b9f643c75d86b0c52d
Aadesh Goyal <goyalaadesh1998@gmail.com>
2021-11-27 16:28:03 +0530

    Add private file for core bussiness logic and add url and method in view function

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+
+from playground import private
+from playground import public
+from playground import constants
+
+def get_data(request):
+    
+    if request.method == 'GET':
+        page_number = request.GET.get('page_number')
+        search_term = request.GET.get('search_term') or ''
+        data_for_videos = private.get_data_of_videos(search_term)
+        data_for_current_page = public.get_data_for_current_page(data_for_videos, page_number, constants.DATA_PER_PAGE)
+        response = {
+            'status': 200,
+            'data': data_for_current_page,
+        }
+        return HttpResponse(data_for_current_page)
+    
+    else:
+        status = 400
+        message = 'Bad Request'
+        response = {'status': status, 'message': message}
+        return HttpResponse(response)
