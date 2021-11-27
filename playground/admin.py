badd60deca74589ce693fe50a61db61bb8ce9197
Aadesh Goyal <goyalaadesh1998@gmail.com>
2021-11-27 16:19:58 +0530

    Add admin pannel for model

--- playground/admin.py
+++ playground/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from playground import models
+
+@admin.register(models.VideoModel)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publishing_datetime')
+    search_fields = ('title',)
+    list_filter = ('publishing_datetime',)
+    ordering = ('publishing_datetime',)
+    readonly_fields = ('created_at', 'updated_at', 'publishing_datetime', 'youtube_id')
